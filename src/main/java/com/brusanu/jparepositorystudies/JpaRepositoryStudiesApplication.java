2fbc9c53ab0427fb9db696db796692b873af5b5b
BrunaSaNu <bruna.santana.0511@gmail.com>
2025-01-28 21:34:54 -0300

    Start project with entities, controllers and repositories

---
+++ src/main/java/com/brusanu/jparepositorystudies/JpaRepositoryStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.brusanu.jparepositorystudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepositoryStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRepositoryStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brusanu/jparepositorystudies/JpaRepositoryStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brusanu.jparepositorystudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRepositoryStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
